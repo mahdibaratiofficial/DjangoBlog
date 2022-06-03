68870fa2af772ac7295620dd6b0e40d3f4fd0256
Mahdi <mahdibarati696@gmail.com>
2022-06-03 16:09:17 +0430

    Config PhotoUpload

--- blog/settings.py
+++ blog/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+        'NAME'    : 'django',                 # <-- UPDATED line 
+        'USER'    : 'root',                     # <-- UPDATED line
+        'PASSWORD': '',              # <-- UPDATED line
+        'HOST'    : 'localhost',                # <-- UPDATED line
+        'PORT'    : '3306',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -120,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL="media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- blog/urls.py
+++ blog/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from blog import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('home/', include('pages.urls')),
-    path('blog/', include('pages.urls')),
-    path('posts/', include('pages.urls')),
-    path('contact/', include('pages.urls')),
-    path('about/', include('pages.urls')),
+    path('', include('pages.urls'))
 ]
+
+if settings.DEBUG :
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
