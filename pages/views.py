dbfe21bdc10cfbe50025026456b988e329050761
Mahdi <mahdibarati696@gmail.com>
2022-06-03 16:15:22 +0430

    Send Posts Parameter to Html File

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from posts.models import Post
 
 def writeHello():
     return "Hello World!"
 # Create your views here.
 
 def Home_Page(request):
-    return render(request, template_name = "home.html")
+    posts=Post.objects.all()
+    return render(request, template_name = "home.html",context={'posts':posts})
 
 def Blog_Page(request):
-    return render(request, template_name = "blog.html")
+    posts=Post.objects.all()
+    return render(request, template_name = "blog.html",context={'posts':posts})
 
 def Contact_Page(request):
     return render(request, template_name = "contact.html")
