3b2f441b86cea594989b03fc5bfe8d2409b7b32b
Mahdi <mahdibarati696@gmail.com>
2022-06-04 12:00:56 +0430

    some Change : Add Pk To posts add font customize theme

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.Home_Page, name = 'home'),
     path('home/', views.Home_Page, name = 'home'),
     path('blog/', views.Blog_Page, name = 'blog'),
-    path('posts/', views.Posts_Page, name = 'posts'),
+    path('post/<int:pk>/', views.Posts_Page, name = 'post'),
     path('contact/', views.Contact_Page, name = 'contact'),
     path('about/', views.About_Page, name = 'about'),
 ]
